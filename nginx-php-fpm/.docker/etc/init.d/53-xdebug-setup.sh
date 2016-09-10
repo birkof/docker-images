3eb83c9adb8fa599beb04aa89de79318632bb6ea
Daniel STANCU <daniel.stancu@me.com>
2016-09-11 00:18:01 +0300

    Added remote_host environment variable

--- nginx-php-fpm/.docker/etc/init.d/53-xdebug-setup.sh
+++ nginx-php-fpm/.docker/etc/init.d/53-xdebug-setup.sh
@@ -7,10 +7,15 @@ else
     
     echo "xdebug.remote_enable=$XDEBUG_ENABLE" >> ${xdebug_ini} \
     && echo "xdebug.remote_autostart=off" >> ${xdebug_ini} \
-    && echo "xdebug.remote_connect_back=on" >> ${xdebug_ini} \
     && echo "xdebug.remote_port=9000" >> ${xdebug_ini} \
     && echo "xdebug.max_nesting_level=1000" >> ${xdebug_ini} \
     && echo "xdebug.idekey=$XDEBUG_IDEKEY" >> ${xdebug_ini}
+
+    if [ ! -z "${XDEBUG_REMOTE_HOST-}" ]; then
+        echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> ${xdebug_ini}
+    else
+        echo "xdebug.remote_connect_back=on" >> ${xdebug_ini}
+    fi
     
     echo -e  "\033[1;38;5;203m[Xdebug] Set Xdebug with remote_enable $XDEBUG_ENABLE and idekey: $XDEBUG_IDEKEY."
 fi
